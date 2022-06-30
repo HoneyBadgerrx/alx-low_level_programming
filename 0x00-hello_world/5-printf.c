d0b6f5f6f2c208706f53c90b35a8ccbb75392673
Kabir <Katoyebir@gmail.com>
2022-06-30 10:29:52 -0700

    j

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * function name - printf
+ *
+ * Description: print with printf
+ * section header: main section
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
