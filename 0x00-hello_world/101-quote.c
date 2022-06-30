58220cb0db4e3b5066c737bb62e922ece068a886
Kabir <Katoyebir@gmail.com>
2022-06-30 13:47:27 -0700

    d

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,19 @@
-#include <unistd.h> 
+#include <unistd.h>
+#include <sys/syscall.h>
 
-#include <sys/syscall.h> 
-
+/**
+ * main - entry point
+ * function_name - no_print
+ *
+ * Description: Prints without printf/puts
+ * section header: main
+ * Return: 1
+ */
 int main(void)
 {
-	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	syscall(SYS_write, 1, msg, sizeof(msg));
-	return 1;
+	const char msg[] =
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-} 
+	syscall(SYS_write, 1, msg, sizeof(msg));
+	return (1);
+}
