20080d46c8fc14de3051750a5830fc3593193332
Kabir <Katoyebir@gmail.com>
2022-06-30 09:37:32 -0700

    q

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * return = 0, successful
+ */
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+	return (0);
+}
