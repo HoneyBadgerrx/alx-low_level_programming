d2533c01cb5813feeb74813919df2c75d8052832
Kabir <Katoyebir@gmail.com>
2022-07-01 05:11:48 -0700

    a

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,14 @@
  */
 int main(void)
 {
-	char (kabir) [] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-		'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-		 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	int x = 'a';
+	int y = 'z';
 
-	putchar(kabir);
+	while (x <= y)
+	{
+		putchar(x);
+		++x;
+	}
 	putchar('\n');
 	return (0);
 }
