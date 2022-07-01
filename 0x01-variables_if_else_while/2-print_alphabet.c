62a48ab368c5fd6f189cadf2ce95b9d0323de536
Kabir <Katoyebir@gmail.com>
2022-07-01 04:16:22 -0700

    q

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * function_name - print_abc
+ *
+ * Description: prints alphabets
+ * Return: 0
+ */
+int main(void)
+{
+	char (kabir) [] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+		'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+		 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+	putchar(kabir);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Each of this is a c program
