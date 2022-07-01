9d37eb970a58075058527777a1033b014daf97f1
Kabir <Katoyebir@gmail.com>
2022-07-01 05:49:25 -0700

    q

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * function_name - numbr
+ *
+ * Description: print num
+ * Return: 0
+ */
+int main(void)
+{
+	int x = 0;
+	int y = 10;
+
+	while (x <= y)
+	{
+		printf("%d", x);
+		++x;
+	}
+	putchar('\n');
+	return (0);
+}
