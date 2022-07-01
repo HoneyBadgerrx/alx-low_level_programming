337ba482bd2776595635b956c0da23665292f134
Kabir <Katoyebir@gmail.com>
2022-07-01 13:06:45 -0700

    q

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 /**
  * main - entry point
- * function_ name - print_lnum
+ * function_ name - print_3dignum
  *
- * Description: print least possible comb of 2 num
+ * Description: print least possible comb of 3 num
  * Return: 0
  */
 int main(void)
 {
 	int x = '0';
 	int y;
+	int z;
 	int max_x = '8';
 	int max_y = '9';
+	int max_z - '9';
 
 	while (x <= max_x)
 	{
