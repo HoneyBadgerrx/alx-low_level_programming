041b99d1d56f12e556d47ff78363299cf995dd0e
Kabir <Katoyebir@gmail.com>
2022-07-01 06:08:48 -0700

    d

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * function_name - putchar_num
+ *
+ * Description: print char of num
+ * Return: 0
+ */
+int main(void)
+{
+	int x = '0';
+	int y = '10';
+
+	while (x < y)
+	{
+		putchar(x);
+		++x;
+	}
+	putchar('\n');
+	return (0);
+}
