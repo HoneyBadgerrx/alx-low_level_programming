7d41c0ca7f1b040c3c4942e78c34251486ed9566
Kabir <Katoyebir@gmail.com>
2022-07-05 06:10:00 -0700

    d

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,12 @@
 /**
   * main - entry point
-  * function_name - print_putchar
+  * function_name - put_char
   *
-  * Description: Prints putchar
+  * Description:
   * Return: 0
   */
-
-void print_putchar()
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-}
 int main(void)
 {
-	print_putchar();
+	_putchar(_);
 	return (0);
 }
