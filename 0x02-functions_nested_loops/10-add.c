e5abf90f800b022f3e218453229e4c5ab0aed67f
Kabir <Katoyebir@gmail.com>
2022-07-06 13:03:00 -0700

    q

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * add - prints sum
+  *
+  * Description: print sum
+  * @b: any numeral
+  * @a: any numeral
+  * Return: int
+  **/
+
+/*add 2 digits*/
+int add(int a, int b)
+{
+	int sum = a + b;
+	return(sum);
+}
