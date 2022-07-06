3fa67577ef2a5412ae378daecce9f02e77c97f9c
Kabir <Katoyebir@gmail.com>
2022-07-06 15:49:26 -0700

    p

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,19 +9,6 @@
 /*times table*/
 void print_times_table(int n)
 {
-	int n = 0;
-	int i = 0;
-
-	while (i < 10)
-	{
-		_putchar(a);
-		a += i;
-		if ((i < 10))
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-		_putchar('\n');
-		++i;
-	}
+	_putchar('\n');
+	return (n);
 }
