70b825cd53bae2e95d12c8d2e602ea465b44223d
Kabir <Katoyebir@gmail.com>
2022-07-06 15:47:53 -0700

    p

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,13 +2,14 @@
 /**
   * times_table - times table
   *
+  * @n: does stuff
   * Return: void
   **/
 
 /*times table*/
 void print_times_table(int n)
 {
-	int a = 0;
+	int n = 0;
 	int i = 0;
 
 	while (i < 10)
