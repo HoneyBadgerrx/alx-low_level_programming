f3e9e7817149d7f9e8d2a0d5a7672ff921ec1283
Kabir <Katoyebir@gmail.com>
2022-07-06 04:01:37 -0700

    g

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * _print_last_digit - printa the last digit
+  *
+  * Description: print final digit
+  * @c: any numeral
+  * Return: int
+  **/
+
+/*print abs*/
+int print_last_digit(int c);
+{
+	int mod;
+	mod = c % 10;
+
+	return(mod);
+}
