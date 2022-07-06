3a6cb3d56d6dd05d8f81bc23920391f6da697792
Kabir <Katoyebir@gmail.com>
2022-07-06 03:42:22 -0700

    q

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+  * print_sign - check for sign
+  *
+  * Description: check for sign
+  * @n: any numeral
+  * Return: int
+  **/
+
+/*print sign*/
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n = 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+}
