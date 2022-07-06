0e044f141b5a29d89a96fca0b98bd913255a5870
Kabir <Katoyebir@gmail.com>
2022-07-06 15:44:06 -0700

    p

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  * times_table - times table
+  *
+  * Return: void
+  **/
+
+/*times table*/
+void times_table(void)
+{
+	int a = 0;
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(a);
+		a += i;
+		if ((i < 10))
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+		++i;
+	}
+}
