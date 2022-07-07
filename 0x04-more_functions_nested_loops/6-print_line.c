d2caf5bc896aa22a90afd2107fab11b4c234564e
Kabir <Katoyebir@gmail.com>
2022-07-07 12:11:07 -0700

    q

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_line - prints lines
+ *
+ * @n: any numeral
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int a = 0;
+
+	if (n <= 0)
+		_putchar('\n');
+	while (a <= n)
+	{
+		_putchar('_');
+		_putchar('\n');
+	}
+}
