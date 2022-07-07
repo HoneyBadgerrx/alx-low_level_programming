e1f75d87cc5f9028ef506cd49a5118a6eda7dc59
Kabir <Katoyebir@gmail.com>
2022-07-07 14:43:47 -0700

    a

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_square - prints squares
+ *
+ * @size: any numeral
+ * Return: nil
+ */
+void print_square(int size)
+{
+	int a = 1;
+	int b = 1;
+
+	if (size > 0)
+	{
+		while (size > 0)
+		{
+			a = 1;
+
+			while (a <= size)
+			{
+				_putchar('#');
+				++a;
+			}
+			_putchar('\n');
+			--size;
+		}
+
+	}
+	else
+		_putchar('\n');
+}
