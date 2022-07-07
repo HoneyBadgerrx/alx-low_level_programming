4b775bad933c234e5b616bb88ff418b43883fe6c
Kabir <Katoyebir@gmail.com>
2022-07-07 11:09:46 -0700

    i

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * mul - mult int
+  *
+  * @b: any numeral
+  * @a: any numeral
+  * Return: int
+  **/
+
+/*mul 2 digits*/
+int mul(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
