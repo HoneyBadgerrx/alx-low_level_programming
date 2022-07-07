9242bd71f6bbe152c2bf444e46fd14cabcc4afab
Kabir <Katoyebir@gmail.com>
2022-07-07 14:13:39 -0700

    l

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonals
+ *
+ * @n: any numeral
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	int a = 1;
+	int b = 1;
+
+	if (n > 0)
+	{
+		while (a <= n)
+		{
+			b = 1;
+
+			while (b <= a)
+			{
+				_putchar(' ');
+				--a;
+			}
+			_putchar('\\');
+			_putchar('\n');
+			++a;
+		}
+	}
+	else
+		_putchar('\n');
+}
