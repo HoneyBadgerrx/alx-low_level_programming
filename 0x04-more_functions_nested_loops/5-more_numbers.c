c404cdfbb3e017720a494392a81aa3b550a1abd1
Kabir <Katoyebir@gmail.com>
2022-07-07 11:43:54 -0700

    q

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 1-14
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int a = 0;
+	int b = 0;
+
+	while (a < 2)
+	{
+		while (b < 10)
+		{
+			if ((a >= 1) && (b >=5)
+					break;
+			if (a = 1)
+				_putchar('0' + a);
+			_putchar('0' + b);
+			++b;
+		}
+		++a;
+	}
+	_putchar('\n');
+}
