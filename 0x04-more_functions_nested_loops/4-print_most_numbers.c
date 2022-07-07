88faa67777ab106a10a1a4b53ea3693eda4abbb4
Kabir <Katoyebir@gmail.com>
2022-07-07 11:22:38 -0700

    v

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print most nums
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if ((i != 2) | (i != 4))
+		{
+			_putchar('0' + i);
+		}
+		++i;
+	}
+	_putchar('\n');
+}
