f0c8cf13fe08c3d1d9e83446e3d6c8119d444e06
Kabir <Katoyebir@gmail.com>
2022-07-08 04:26:53 -0700

    o

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,21 +1,26 @@
 #include "main.h"
+#include <limits.h>
 
 /**
- * print_number - prints int with _putchar
+ * print_number - prints int with putchar
  *
  * @n: any int
  * Return: nil
  */
 void print_number(int n)
 {
-	if (n < 0)
+/*	if (n < 0)
 	{
 		while (n < 0)
 			n = n * -1;
 		putchar('-');
 	}
-
+*/
 	if (n / 10)
 		print_number(n / 10);
-	_putchar('0' + (n % 10));
+	putchar('0' + (n % 10));
+}
+void main(void)
+{
+	print_number(INT_MIN);
 }
