7f6e80d492591588a80418c25a3d8256b35e6402
Kabir <Katoyebir@gmail.com>
2022-07-07 17:02:53 -0700

    l

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_number - prints int with putchar
+ *
+ * @n: any int
+ * Return: nil
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+	
+	if (n / 10)
+		print_number(n / 10);
+	_putchar('0' + (n % 10));
+}
