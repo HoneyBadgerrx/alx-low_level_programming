935497f4f3d6c20b8bd1fe8884b78c2e209b23b6
Kabir <Katoyebir@gmail.com>
2022-07-09 04:16:06 -0700

    l

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,17 +10,34 @@ void times_table(void)
 {
 	int a = 0;
 	int i = 0;
+	int n = 9;
+	int mul;
 
-	while (i < 10)
+	while (i <= n)
 	{
-		_putchar(a);
-		a += i;
-		if ((i < 10))
+		a = 0;
+
+		while (a <= n);
 		{
+			mul = i * a;
+
+			if ((mul > 9) && (mul < 100))
+			{
+				_putchar('0' + ((mul / 10) % 10));
+				_putchar('0' + (mul % 10));
+			}
+			else if ((mul > 99) && (mul < 1000))
+			{
+				_putchar('0' + ((mul / 100) % 10));
+				_putchar('0' + ((mul / 10) % 10));
+				_putchar('0' + (mul % 10));
+			}
+			else
+				_putchar('0' + mul);
 			_putchar(',');
 			_putchar(' ');
+			_putchar(' ');
+			++a;
 		}
-		_putchar('\n');
 		++i;
 	}
-}
