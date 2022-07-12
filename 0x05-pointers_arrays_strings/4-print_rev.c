bffebc40c5445c60d9fc8f419afbc7660b02fd5c
Kabir <Katoyebir@gmail.com>
2022-07-12 14:31:22 -0700

    k

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - string in reverse
+ *
+ * @s: string
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int len = strlen(s);
+	int i = 0;
+
+	while ((len - i) >= 0)
+	{
+		printf("%c", s[len - i]);
+		++i;
+	}
+}
