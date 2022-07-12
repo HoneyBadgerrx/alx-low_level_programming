b2c0698f3ffcd7f7594c08ff8eef5b7862fa12a7
Kabir <Katoyebir@gmail.com>
2022-07-12 15:48:42 -0700

    a

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,10 +10,10 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	while (i <= n)
+	while (i <= (n - 1))
 	{
 		printf("%d", a[i]);
-		if (i != n)
+		if (i != (n = 1))
 		{
 			putchar(',');
 			putchar(' ');
