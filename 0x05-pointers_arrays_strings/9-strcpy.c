55f77f11639172d923bb1eabc50ccdd10384727e
Kabir <Katoyebir@gmail.com>
2022-07-12 16:39:24 -0700

    q

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strcpy - copies str from src to dest
+ *
+ * @source: source str
+ * @dest: dest str
+ * Return: 0
+ */
+char *_strcpy(char *dest, char *src);
+{
+	*dest = &src;
+
+	return(*dest);
+}
