3762bee2ab1d1ed20327391cdb11314e89a066a7
Kabir <Katoyebir@gmail.com>
2022-07-14 14:03:07 -0700

    wordupper

---
+++ 0x06-pointers_arrays_strings/6-capstring.c
@@ -0,0 +1,19 @@
+include "main.h"
+/**
+ * cap_string - cpas evry word
+ *
+ * @src: source string
+ * Return: char
+ */
+
+char *cap_string(char *)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; ++i)
+	{
+		if (str[index - 1] == ' ' || str[index - 1] == '\t' || str[index - 1] == '\n' || str[index - 1] == ',' || str[index - 1] == ';' || str[index - 1] == '.' || str[index - 1] == '!' || str[index - 1] == '?' || str[index - 1] == '"' || str[index - 1] == '(' || str[index - 1] == ')' || str[index - 1] == '{' || str[index - 1] == '}')
+			src[i] -= 32;
+	}
+	return (src);
+}
