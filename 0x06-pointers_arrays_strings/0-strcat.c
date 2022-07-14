74e92eac0530293d049d3c89f0f43202f8d98904
Kabir <Katoyebir@gmail.com>
2022-07-14 05:32:08 -0700

    l

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,4 +7,15 @@
  * @src: source string
  * Return: char
  */
-char *_strcat(char *dest, char *src) = &strcat
+char *_strcat(char *dest, char *src)
+{
+	int dlen = strlen(dest);
+	int i;
+
+	for (i = 0; src[i] != '\n' ; i++)
+	{
+		dest[dlen + i] = src[i];
+	}
+	dest[dlen + i] = '\0';
+	return (dest);
+}
