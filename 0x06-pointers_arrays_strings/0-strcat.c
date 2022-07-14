8cd4a4a5036c1fc924ea221ba8b17b8329b60f94
Kabir <Katoyebir@gmail.com>
2022-07-14 02:48:03 -0700

    k

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate string
+ *
+ * @dest: destination str
+ * @src: source string
+ * Return: char
+ */
+char *_strcat(char *dest, char *src) = &strcat
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *_strcat(char *dest, char *src);
+
+#endif /* main_h */
