f6c897bc8f95b8e42d3ff6ef33f8e985ea8e1a22
Kabir <Katoyebir@gmail.com>
2022-07-18 04:36:11 -0700

    j

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _memcpy - copies mem content
+ *
+ * @dest: destination memory
+ * @src: source memory
+ * @n: amount of bytes to copy
+ * Return: pointer to dest
+ */
+
+
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; ++i)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strchr - find char c in string
+ *
+ * @c: character to search for
+ * @s: string to be searched
+ * Return: pointer to char c
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	char* p;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		if (s[i] == c)
+		{
+			p = &s[i];
+			break;
+	}
+	return (p);
+}
