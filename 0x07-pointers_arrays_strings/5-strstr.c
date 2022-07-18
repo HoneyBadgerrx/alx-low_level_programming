8cde2e3d65265093176e358c6d671af3db938071
Kabir <Katoyebir@gmail.com>
2022-07-18 12:47:23 -0700

    task 7

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strstr - checks for needle string in str haystack
+ *
+ * @haystack: string to be searched
+ * @needle: string to search for
+ * Return: pointer to needle in hay
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int v;
+	int counter;
+	int size = 4;
+
+	for (i = 0; haystack[i] != '\0'; ++i)
+	{
+		if (haystack[i] == needle[0])
+		{
+			v = 0;
+			counter = 0;
+			while (haystack[i + v] == needle[v])
+			{
+				++counter;
+				++v;
+			}
+			if (counter == size)
+				return (haystack + i);
+		}
+	}
+	return ('\0');
+}
