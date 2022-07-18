cd5a18ac781895274de14d00f196139954b422ee
Kabir <Katoyebir@gmail.com>
2022-07-18 06:09:26 -0700

    3strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strspn - max bytes where acceot can be found in s
+ *
+ * @s: character to search in
+ * @accept: string to be searched for
+ * Return: unsigned int
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i;
+	unsigned int v;
+	unsigned int p = o;
+
+	for (i = 0; accept[i] != '\0'; ++i)
+	{
+		for (v = 0; s[v] != '\0'; ++v)
+		{
+			if (accept[i] == s[v] && p < v)
+				p = v;
+		}
+	}
+	return (p + 1);
+}
