1188016c5abcd58279e8f43b1103d9008976b0f0
Kabir <Katoyebir@gmail.com>
2022-07-19 12:11:53 -0700

    done

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -30,22 +30,3 @@ char *_strpbrk(char *s, char *accept)
 	}
 	return (p);
 }
-int main(void)
-
-{
-
-		char *s = "First, solve the problem. Then, write the code.";
-
-			char *f = "School";
-
-				char *t;
-
-
-
-					t = _strpbrk(s, f);
-
-						printf("%s\n", (t == NULL ? "nil" : t));
-
-							return (0);
-
-}
