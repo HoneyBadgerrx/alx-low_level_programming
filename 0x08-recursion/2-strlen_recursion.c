2ff63c09f4532f449685bf90081e6dbca78c6567
Kabir <Katoyebir@gmail.com>
2022-07-20 07:09:57 -0700

    p

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - return string length
+ *
+ * @s: string
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s == '\0')
+		return (0);
+	++len;
+	len += _strlen_recursion(s + 1);
+	return(len);
+}
