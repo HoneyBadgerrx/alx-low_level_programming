a0ad4617f51b5a21411e6459a3b8accf1dd609b5
Kabir <Katoyebir@gmail.com>
2022-07-20 05:30:53 -0700

    2d array

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,21 +7,22 @@
  * @size: size of array
  * Return: void
  */
-void print_diagsums(int **a, int size)
+void print_diagsums(int *a, int size)
 {
 	int sum1 = 0, sum2 = 0, i = 0;
 
 	while (i < size)
 	{
-		sum1 += **a;
-		*a = *a + size + 1;
+		sum1 += *a;
+		a = a + size + 1;
 		++i;
 	}
-	*a = *a - size + 1;
+	a = a - size - 1;
+	a = a - size + 1;
 	while (i > 0)
 	{
-		sum2 += **a;
-		*a = *a - size - 1;
+		sum2 += *a;
+		a = a - size + 1;
 		--i;
 	}
 	printf("%d, %d", sum1, sum2);
