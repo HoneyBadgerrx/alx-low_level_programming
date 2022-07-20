e15eff6b30ccb8f87fc570c1fa8aa01d22c4247a
Kabir <Katoyebir@gmail.com>
2022-07-20 05:36:00 -0700

    final task

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - sets address of strings
+ *
+ * @s: string to be copied to
+ * @to: copied string
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
