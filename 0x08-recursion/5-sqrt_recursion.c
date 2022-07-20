34be0b67c8930a918d315625b20f3324955510d1
Kabir <Katoyebir@gmail.com>
2022-07-20 08:09:09 -0700

    betty for 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,18 +1,25 @@
 #include "main.h"
 /**
+ * helperfunc - used to check for root
  *
- *
- *
+ * @n: number to find sqrt
+ * @root: possible rot of n
+ * Return: either error or root
  */
 int helperfunc(int n, int root)
 {
 	if (root * root == n)
-		return(root);
+		return (root);
 	if (root >= n / 2)
 		return (-1);
 	return (helperfunc(n, root + 1));
 }
-
+/**
+ * _sqrt_recursion - finds sqrt of n using recursion
+ *
+ * @n: number with unknown sqrt
+ * Return: error or sqrt of n
+ */
 int _sqrt_recursion(int n)
 {
 	int root = 0;
