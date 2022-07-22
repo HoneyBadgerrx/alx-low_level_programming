cfb4b4d348f0e429c2f88f3aeacf62c79610ff81
Kabir <Katoyebir@gmail.com>
2022-07-22 13:50:36 -0700

    task 4

---
+++ 0x09-static_libraries/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds positive integers
+ *
+ * @argc: number of arguments
+ * @argv: commandline arguments
+ * Return: 0 if successfyl
+ */
+int main(int argc, char **argv)
+{
+	int i, j, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (1);
+	}
+	for (i = 1; i < argc; ++i)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			++j;
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}	
