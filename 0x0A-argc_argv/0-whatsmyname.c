624d376c4c2ba04d24462952520303fb2c521f52
Kabir <Katoyebir@gmail.com>
2022-07-22 11:07:14 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - print prog name
+ *
+ * @argc: program count
+ * @argv: program arg
+ * Return: 0 on success
+ */
 int main(int argc, char **argv)
 {
 	(void)argc;
