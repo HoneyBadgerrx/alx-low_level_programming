68eead71c64b95fc342472e8ed208307a8637251
Kabir <Katoyebir@gmail.com>
2022-07-26 05:20:12 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,24 +8,28 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0, size;
+	int i = 0, total, sizes1, sizes2;
 	char *p;
 
 	if (s1 == NULL)
-		s1[0] = '\0';
+		sizes1 = 0;
+	else
+		sizes1 = strlen(s1);
 	if (s2 == NULL)
-		s2[0] = '\0';
-	size = strlen(s1) + strlen(s2) + 1;
-	p = malloc(sizeof(char) * size);
+		sizes2 = 0;
+	else
+		sizes2 = strlen(s2);
+	total = sizes1 + sizes2 + 1;
+	p = malloc(sizeof(char) * total);
 	if (p == NULL)
 		return (NULL);
-	while (*s1)
+	while (*s1 && s1 != NULL)
 	{
 		p[i] = *s1;
 		++i;
 		++s1;
 	}
-	while (*s2)
+	while (*s2 && s2 != NULL)
 	{
 		p[i] = *s2;
 		++i;
@@ -33,4 +37,89 @@ char *str_concat(char *s1, char *s2)
 	}
 	p[i] = '\0';
 	return (p);
+}
+#include <stdio.h>
+
+#include <stdlib.h>
+
+
+
+char *str_concat(char *, char *);
+
+
+
+/**
+ *
+ *  * main - check the code .
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
+int main(void)
+
+{
+
+		char *s;
+
+
+
+			s = str_concat("Hello", NULL);
+
+				if (s == NULL)
+
+						{
+
+									printf("failed\n");
+
+											return (1);
+
+												}
+
+					printf("%s\n", s);
+
+						free(s);
+
+
+
+						    s = str_concat(NULL, "Hello");
+
+						    	if (s == NULL)
+
+									{
+
+												printf("failed\n");
+
+														return (1);
+
+															}
+
+								printf("%s\n", s);
+
+									free(s);
+
+
+
+									    s = str_concat(NULL, NULL);
+
+									    	if (s == NULL)
+
+												{
+
+															printf("failed\n");
+
+																	return (1);
+
+																		}
+
+											printf("%s\n", s);
+
+												free(s);
+
+
+
+													return (0);
+
 }
