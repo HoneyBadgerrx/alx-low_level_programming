9e19b3335c7431fd074995d0f40a2a428a038af8
Kabir <Katoyebir@gmail.com>
2022-07-26 04:44:49 -0700

    typo in while*s2 loop

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -29,8 +29,45 @@ char *str_concat(char *s1, char *s2)
 	{
 		p[i] = *s2;
 		++i;
-		++s1;
+		++s2;
 	}
 	p[i] = '\0';
 	return (p);
+}
+/**
+ *
+ *  * main - check the code for ALX School students.
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
+int main(void)
+
+{
+
+	    char *s;
+
+
+
+	        s = str_concat("Betty ", "Holberton");
+
+		    if (s == NULL)
+
+			        {
+
+					        printf("failed\n");
+
+						        return (1);
+
+							    }
+
+		        printf("%s\n", s);
+
+			    free(s);
+
+			        return (0);
+
 }
