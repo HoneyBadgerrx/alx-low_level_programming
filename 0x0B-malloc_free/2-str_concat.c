fe2675cf6daca1da257b4fa10f1deebf67a4f817
Kabir <Katoyebir@gmail.com>
2022-07-26 04:48:35 -0700

    changed *s1 and *s2 to s1[0] s2[0]

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,9 +12,9 @@ char *str_concat(char *s1, char *s2)
 	char *p;
 
 	if (s1 == NULL)
-		*s1 = '\0';
+		s1[0] = '\0';
 	if (s2 == NULL)
-		*s2 = '\0';
+		s2[0] = '\0';
 	size = strlen(s1) + strlen(s2) + 1;
 	p = malloc(sizeof(char) * size);
 	if (p == NULL)
