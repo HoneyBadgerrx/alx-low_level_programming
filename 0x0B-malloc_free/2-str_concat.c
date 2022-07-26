e2e66020eadd2c967a774343a20311a68661c4f1
Kabir <Katoyebir@gmail.com>
2022-07-26 04:23:28 -0700

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include "mian.h"
+/**
+ * str_concat - concatenates 2 given strings
+ *
+ * @s1: first string
+ * @s2: following string
+ * Return: pointer to concat string in memory
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, size;
+	char *p;
+
+	if (s1 == NULL)
+		*s1 = "";
+	if (s2 == NULL)
+		*s2 = "";
+	size = strlen(s1) + strlen(s2) + 1;
+	p = malloc(sizeof(char) * size);
+	if (p = NULL)
+		return (NULL);
+	while (*s1)
+	{
+		p[i] = *s1;
+		++i;
+		++s1;
+	}
+	while (*s2)
+	{
+		p[i] = *s2;
+		++i;
+		++s1;
+	}
+	p[i] = '\0';
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,5 +8,6 @@
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 void simple_print_buffer(char *buffer, unsigned int size);
+char *str_concat(char *s1, char *s2);
 
 #endif /* main_h */
