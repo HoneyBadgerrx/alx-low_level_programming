d1f6b526afdc8ebd6354a78fedf7e53c3b8ace59
Kabir <Katoyebir@gmail.com>
2022-07-27 02:33:23 -0700

    task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * free_grid - frees a 2d array
+ *
+ * @grid: pointer to 2d array
+ * @height: height of the array
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid ==NULL || height <= 0)
+		return (NULL);
+	for (i = 0; i < height; ++I)
+		free(grid[i]);
+	free (grid);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -10,5 +10,6 @@ char *_strdup(char *str);
 void simple_print_buffer(char *buffer, unsigned int size);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 
 #endif /* main_h */
