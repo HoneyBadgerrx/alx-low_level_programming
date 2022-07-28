8b4b3d671f51a67d6595f2c0756c567bc85b85af
Kabir <Katoyebir@gmail.com>
2022-07-28 11:51:04 -0700

    removed init p

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,7 +9,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i, total;
-	int *p;
+	void *p;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -17,7 +17,5 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	p = malloc(total);
 	if (p == NULL)
 		return (NULL);
-	for (i = 0; i < total; ++i)
-		p[i] = 0;
 	return (p);
 }
