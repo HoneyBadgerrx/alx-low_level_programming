8d7acc168398819485a93234870da2ec2bfdc322
Kabir <Katoyebir@gmail.com>
2022-07-28 11:45:02 -0700

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _calloc - creates memory with malloc and inits to 0
+ *
+ * @nmemb: number of size to create
+ * @size: size of each elements
+ * Return: pointer to created memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i, total;
+	int *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	total = nmemb * size;
+	p = malloc(total);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < total; ++i)
+		p[i] = 0;
+	return (p);
+}
