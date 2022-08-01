d36d77d6be8ea172fb63edb0951300ae755c2d4a
Kabir <Katoyebir@gmail.com>
2022-08-01 06:46:19 -0700

    dog_h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Directory containing typedef and structure tasks
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure of dogs characteristics
+ *
+ * @name: name of dog
+ * @age: age of dog in human years
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* dog_h */
