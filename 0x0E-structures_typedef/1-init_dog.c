512379d6ba601255afb806e1a4961e341d03506b
Kabir <Katoyebir@gmail.com>
2022-08-01 06:56:35 -0700

    task 1 init dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+/**
+ * init_dog - initialises dog struct
+ * @d: structure to be init
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
