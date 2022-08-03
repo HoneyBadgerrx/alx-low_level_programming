700c2a33903603bfa030ca42bb559671186a4a48
Kabir <Katoyebir@gmail.com>
2022-08-03 09:53:00 -0700

    added comment

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -8,6 +8,12 @@ void printer(char *name)
 {
 	printf("Hello, my name is %s\n", name);
 }
+/**
+ * print_name - calls printer function
+ * @name: string to be printed
+ * @f: function to print
+ * Return: void
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
