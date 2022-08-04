564cac36adad538bfe52ba935dab549836baf153
Kabir <Katoyebir@gmail.com>
2022-08-04 05:48:40 -0700

    fixed separator conditon

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,13 +10,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list vv;
 
-	if (separator == NULL || n <= 0)
-		return;
 	va_start(vv, n);
 	for (i = 0; i < n; ++i)
 	{
 		printf("%d", va_arg(vv, int));
-		if (i  != n - 1)
+		if (i  != n - 1 && separator)
 			printf("%s", separator);
 	}
 	printf("\n");
