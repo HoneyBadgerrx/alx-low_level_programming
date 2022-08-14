3e4b1564094eded6112d1a7703d7938ea92dea21
Kabir <Katoyebir@gmail.com>
2022-08-14 05:19:13 -0700

    task 0 printlist

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "list.h"
+/**
+ * print_list - prints all elements of a list
+ * @h: pointer to head of list
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
