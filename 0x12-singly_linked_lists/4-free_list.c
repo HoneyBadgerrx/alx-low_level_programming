798441b1357436890dc8139301c49cd992d30561
Kabir <Katoyebir@gmail.com>
2022-08-14 08:38:19 -0700

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * free_list - frees a list
+ * @head: start of list
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	if (head->next)
+		free_list(head->next);
+	free(head);
+}
