dd8c5a075ac02f747eecdd8ee21b77bbd5412467
Kabir <Katoyebir@gmail.com>
2022-08-14 07:08:32 -0700

    task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * add_node - adds node tolist
+ * @head: node to be aded to
+ * @str: string to be duplicated
+ * Return: null if failes, address if successful
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *i = malloc(sizeof(list_t));
+
+	if (i == NULL)
+		return (NULL);
+	i->str = malloc(strlen(str));
+	i->str = strdup(str);
+	if (*head != NULL)
+		i->next = (**head).next;
+	else
+		i->next = NULL;
+	return (i);
+}
