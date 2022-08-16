4549f15696222dfa73162de497212e03886dde88
Kabir <Katoyebir@gmail.com>
2022-08-16 08:49:56 -0700

    fixed typo

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (*head == NULL)
 		return (-1);
-	for (i = 0; i != idx; ++i)
+	for (i = 0; i != index; ++i)
 	{
 		if (j == NULL)
 			return (-1);
