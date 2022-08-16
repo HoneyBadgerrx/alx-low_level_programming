961456523b3204eb97006a329ac5db958908b2d1
Kabir <Katoyebir@gmail.com>
2022-08-16 09:00:36 -0700

    task 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,8 +7,13 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	unsigned int count = 0;
-	const listint_t *i = head;
+	const listint_t *i;
 
+	if (head == NULL)
+		exit(98);
+	printf("%d\n", head->n);
+	count++;
+	i = head->next;
 	while (i != NULL)
 	{
 		if (i == head)
