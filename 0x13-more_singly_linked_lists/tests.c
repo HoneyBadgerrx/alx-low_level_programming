752e628dc9ca3be6ddcdf685c81b5b2375102393
Kabir <Katoyebir@gmail.com>
2022-08-19 04:23:15 -0700

    fixed order of head check

--- 0x13-more_singly_linked_lists/tests.c
+++ 0x13-more_singly_linked_lists/tests.c
@@ -1,8 +1,6 @@
-#include <stdlib.h>
-
-#include <string.h>
-
 #include <stdio.h>
+
+#include <stdlib.h>
 
 #include "lists.h"
 
@@ -10,7 +8,7 @@
 
 /**
  *
- *  * main - check the code
+ *  * main - check the code .
  *
  *   *
  *
@@ -22,54 +20,10 @@ int main(void)
 
 {
 
-	    listint_t *head;
+		free_listint2(NULL);
 
-	        listint_t *head2;
+			printf("Freed !\n");
 
-		    listint_t *node;
-
-
-
-		        head2 = NULL;
-
-			    add_nodeint(&head2, 0);
-
-			        add_nodeint(&head2, 1);
-
-				    add_nodeint(&head2, 2);
-
-				        add_nodeint(&head2, 3);
-
-					    add_nodeint(&head2, 4);
-
-					        add_nodeint(&head2, 98);
-
-						    add_nodeint(&head2, 402);
-
-						        add_nodeint(&head2, 1024);
-
-							    print_listint_safe(head2);
-
-							        head = NULL;
-
-								    node = add_nodeint(&head, 0);
-
-								        add_nodeint(&head, 1);
-
-									    add_nodeint(&head, 2);
-
-									        add_nodeint(&head, 3);
-
-										    add_nodeint(&head, 4);
-
-										        node->next = add_nodeint(&head, 98);
-
-											    add_nodeint(&head, 402);
-
-											        add_nodeint(&head, 1024);
-
-												    print_listint_safe(head);
-
-												        return (0);
+				return (0);
 
 }
