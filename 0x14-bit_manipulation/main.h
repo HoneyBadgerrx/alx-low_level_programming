a67e670282f16b5eb0f05555536c0ce9f88b467f
Kabir <Katoyebir@gmail.com>
2022-08-19 04:10:11 -0700

    betty fo rtak 0

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,7 +5,7 @@
 
 unsigned int _strlen(const char *s);
 unsigned int binary_to_uint(const char *b);
-unsigned int _pow(int base,unsigned int exp);
+unsigned int _pow(int base, unsigned int exp);
 void print_binary(unsigned long int n);
 int _putchar(char c);
 
