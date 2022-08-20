6b21d41fa0c05f185b192631481817cc473ab4b9
Kabir <Katoyebir@gmail.com>
2022-08-20 05:00:46 -0700

    better code

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,24 +1,5 @@
 #include "main.h"
 /**
- * _pow - finds exponent of base
- * @base: number to be exp
- * @exp: exp number
- * Return: result of exp
- */
-unsigned int _pow(int base, unsigned int exp)
-{
-	int result = base;
-
-	while (exp > 1)
-	{
-		result *= base;
-		--exp;
-	}
-	if (exp == 0)
-		return (1);
-	return (result);
-}
-/**
  * get_bit - gets bit at index
  * @n: bits to check
  * @index: index of required bit
@@ -30,6 +11,6 @@ int get_bit(unsigned long int n, unsigned int index)
 
 	if (index > 31)
 		return (-1);
-	i = _pow(2, index);
-	return (i & n);
+	n >>= index;
+	return (n & 1);
 }
