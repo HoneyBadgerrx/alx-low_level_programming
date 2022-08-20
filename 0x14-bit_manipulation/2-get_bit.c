971580412b615b83b1de0775af81730b6e972777
Kabir <Katoyebir@gmail.com>
2022-08-20 04:46:18 -0700

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,7 +9,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int i;
 
-	if (index > 31 ||| index < 0)
+	if (index > 31 || index < 0)
 		return (-1);
 	i = _pow(2, index);
 	return (i & n);
