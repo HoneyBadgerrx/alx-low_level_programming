6fc9127c294f5742fe52e081857ef5f53c402a89
HoneyBadgerrx <katoyebir@gmail.com>
2022-09-29 06:29:42 -0700

    fix

--- 0x17-doubly_linked_lists/102.py
+++ 0x17-doubly_linked_lists/102.py
@@ -7,8 +7,10 @@ def pal(a):
         if (b[i] != c[i]):
             return 0
     return (int(b))
+q = []
 for a in reversed(range(1, 1000)):
     for b in reversed(range(1, 1000)):
-        if pal(a * b) != 0:
-            print(pal(a * b))
-            sys.exit(1)
+        if a // 100 and b // 100:
+            if pal(a * b) != 0:
+                q.append(pal(a * b))
+print(max(q))
