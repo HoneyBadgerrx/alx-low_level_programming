f351b23ad886a11a65f761906f7916afc380d910
HoneyBadgerrx <katoyebir@gmail.com>
2022-09-29 05:42:22 -0700

    l

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -34,6 +34,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
+	new->n = n;
 	p->prev->next = new;
 	new->next = p;
 	new->prev = p->prev;
