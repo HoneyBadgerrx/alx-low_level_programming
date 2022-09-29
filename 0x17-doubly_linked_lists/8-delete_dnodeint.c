8a7d67ba7e6abceb5fb97a76f34805475874daaf
HoneyBadgerrx <katoyebir@gmail.com>
2022-09-29 06:10:05 -0700

    o

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,8 +10,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int i = 0;
 	dlistint_t *p = *head;
 
-	if (*head == NULL)
-		return (1);
+	if (head === NULL || *head == NULL)
+		return (-1);
 	for (i = 0; i < index; ++i)
 	{
 		if (p == NULL)
