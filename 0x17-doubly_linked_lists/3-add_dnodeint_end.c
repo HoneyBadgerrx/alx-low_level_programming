bc7eda4f92193d89c94efc6a6eb14566f7cfbf64
HoneyBadgerrx <katoyebir@gmail.com>
2022-09-29 04:46:48 -0700

    added return

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -18,6 +18,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		(*head)->n = n;
 		(*head)->prev = NULL;
 		(*head)->next = NULL;
+		return (*head);
 	}
 	while (p->next)
 		p = p->next;
