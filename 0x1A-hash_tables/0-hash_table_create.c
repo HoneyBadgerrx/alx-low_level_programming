280c45f0735354e313b83073f842f7e9dd5dde9f
HoneyBadgerrx <katoyebir@gmail.com>
2022-10-20 12:31:47 -0700

    finish

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,15 +7,19 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *p;
+	unsigned long int i;
 
-	if (size < 0)
-		return (NULL);
 	p = malloc(sizeof(hash_table_t));
 	if (p == NULL)
 		return (NULL);
 	p->size = size;
 	p->array = malloc(sizeof(hash_node_t) * size);
 	if (p->array == NULL)
+	{
+		free(p);
 		return (NULL);
+	}
+	for (i = 0; i < size; ++i)
+		p->array[i] = NULL;
 	return (p);
 }
