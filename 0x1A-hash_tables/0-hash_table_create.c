a566d22a99d55853154e8703825983a6002cbeb5
HoneyBadgerrx <katoyebir@gmail.com>
2022-10-20 12:26:47 -0700

    added null

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *p;
 
+	if (size < 0)
+		return (NULL);
 	p = malloc(sizeof(hash_table_t));
 	if (p == NULL)
 		return (NULL);
