9a13dab9c335a7d43038d0c966d30648a4f4ea40
HoneyBadgerrx <katoyebir@gmail.com>
2022-10-21 03:43:41 -0700

    done

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints hash table
+ * @ht: hast tabke
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *j;
+	int flag = 0;
+
+	if (ht == NULL || ht->array == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; ++i)
+	{
+		while (ht->array[i] == NULL)
+			++i;
+		j = ht->array[i];
+		while (j)
+		{
+			if (flag == 1)
+				printf(", ");
+			printf("\'%s\': \'%s\'", j->key, j->value);
+			flag = 1;
+			j = j->next;
+		}
+	}
+	printf("}\n");
+}
