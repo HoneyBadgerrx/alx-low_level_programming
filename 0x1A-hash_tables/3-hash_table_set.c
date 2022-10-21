d469677706d16064dcd0c9e5b57ad6a7e7027c8b
HoneyBadgerrx <katoyebir@gmail.com>
2022-10-21 02:31:41 -0700

    strdup error

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,74 @@
+#include "hash_tables.h"
+/**
+ * node_create - creates node to reduce side of hash_table_set function
+ * @key: key
+ * @value: value
+ * Return: pointer to created node
+ */
+hash_node_t *node_create(const char *key, const char *value)
+{
+	hash_node_t *p = malloc(sizeof(hash_node_t));
+
+	if (p == NULL)
+		return (NULL);
+	p->key = strdup(key);
+	if (p->key == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	p->value = strdup(value);
+	if (p->value == NULL)
+	{
+		free(p->key);
+		free(p);
+		return (NULL);
+	}
+	p->next = NULL;
+	return (p);
+}
+/**
+ * hash_table_set - sets key and value
+ * @ht: hash table 
+ * @key: key
+ * @value: value
+ * Return: 1 if successful, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *j, *new_node;
+
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+			key == NULL || strlen(key) == 0 || value == NULL)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	j = ht->array[index];
+	if (j == NULL)
+	{
+		new_node = node_create(key, value);
+		j = new_node;
+		return (1);
+	}
+	else
+	{
+		while (j != NULL)
+		{
+			if (strcmp(j->key, key) != 0)
+				j = j->next;
+			else
+			{
+				free(j->value);
+				j->value = strdup(value);
+				if (j->value == NULL)
+					return (0);
+				return (1);
+			}
+		}
+	}
+	j = ht->array[index];
+	new_node = node_create(key, value);
+	new_node->next = j;
+	j = new_node;
+	return (1);
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -38,5 +39,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+hash_node_t *node_create(const char *key, const char *value);
 
 #endif /*HASH_TAB*/
